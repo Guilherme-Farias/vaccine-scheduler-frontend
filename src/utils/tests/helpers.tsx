973254566571db1aba9e3d3c44273d5cc5bf2cf4
Guilherme Farias <guicfarias11@gmail.com>
2022-04-17 18:22:15 -0300

    chore: add faker

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 
 import { render, RenderOptions } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { faker } from '@faker-js/faker';
 
 type CustomRenderProps = Omit<RenderOptions, 'queries'>;
 
@@ -11,4 +12,4 @@ const customRender = (
 ) => render(ui, renderOptions);
 
 export * from '@testing-library/react';
-export { customRender as render, userEvent };
+export { customRender as render, userEvent, faker };
