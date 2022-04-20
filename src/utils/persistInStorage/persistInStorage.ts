efeaf538775aa923a6dc03c54026ce7bf2cd0e23
Guilherme Farias <guicfarias11@gmail.com>
2022-04-20 14:31:34 -0300

    feat: add persistInStorage

---
+++ src/utils/persistInStorage/persistInStorage.ts
@@ -0,0 +1,22 @@
+type Storages = 'localStorage' | 'sessionStorage';
+const STORAGE_KEY = process.env.REACT_APP_STORAGE_KEY;
+
+function getStorageItem<T>(
+  key: string,
+  initialValue: T,
+  storage: Storages = 'localStorage',
+) {
+  const data = window[storage].getItem(`${STORAGE_KEY}_${key}`);
+  return data ? JSON.parse(data) : initialValue;
+}
+
+function setStorageItem<T>(
+  key: string,
+  value: T,
+  storage: Storages = 'localStorage',
+) {
+  const data = JSON.stringify(value);
+  return window[storage].setItem(`${STORAGE_KEY}_${key}`, data);
+}
+
+export { getStorageItem, setStorageItem };
